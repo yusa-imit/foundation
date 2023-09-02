d5ad6878a9ceac36651a582c06583c12c68c1e8c
yusa_imit <esimileiar81@gmail.com>
2023-09-02 20:50:01 +0900

    update for packages

--- packages/eslint-config-custom/library.js
+++ packages/eslint-config-custom/library.js
@@ -1,6 +1,6 @@
-const { resolve } = require("node:path");
+const { resolve } = require('node:path');
 
-const project = resolve(process.cwd(), "tsconfig.json");
+const project = resolve(process.cwd(), 'tsconfig.json');
 
 /*
  * This is a custom ESLint configuration for use with
@@ -13,8 +13,8 @@ const project = resolve(process.cwd(), "tsconfig.json");
 
 module.exports = {
   extends: [
-    "@vercel/style-guide/eslint/node",
-    "@vercel/style-guide/eslint/typescript",
+    '@vercel/style-guide/eslint/node',
+    '@vercel/style-guide/eslint/typescript',
   ].map(require.resolve),
   parserOptions: {
     project,
@@ -24,11 +24,18 @@ module.exports = {
     JSX: true,
   },
   settings: {
-    "import/resolver": {
+    'import/resolver': {
       typescript: {
         project,
       },
     },
   },
-  ignorePatterns: ["node_modules/", "dist/"],
+  rules: {
+    'unicorn/filename-case': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'no-console': 'warn',
+    '@typescript-eslint/consistent-type-imports': 'off',
+  },
+  ignorePatterns: ['node_modules/', 'dist/'],
 };
